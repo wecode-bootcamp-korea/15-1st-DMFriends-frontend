2dbbbe0aa88529c0797e2dfd47e801b01e25f93b
chaebin <cbhan0102@gmail.com>
2020-12-15 16:46:45 +0900

    Fix: eslint windows crlf error resolved

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,10 @@
+import "./Main.scss";
+import { withRouter } from "react-router-dom";
+import React, { Component } from "react";
+
+class Main extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default withRouter(Main);
