ceb531977dc464e98ed0a6a02ebd75ee1cf4adb6
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2020-12-17 13:30:42 +0900

    Add: master에서 새롭게 branch생성 후 초기 세팅 완료

---
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import DetailPageHeader from "../../Components/Header/DetailPageHeader/DetailPageHeader";
+
+import "./ProductDetail.scss";
+
+export class ProductDetail extends Component {
+  render() {
+    return (
+      <>
+        <DetailPageHeader />
+      </>
+    );
+  }
+}
+
+export default ProductDetail;
