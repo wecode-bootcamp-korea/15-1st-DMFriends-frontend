50a7a52560d8ba2352443759a840c1fecf44c9f6
임태진 <imtaejin@imtaejin-ui-MacBookPro.local>
2020-12-18 00:51:35 +0900

    Fix : 리뷰반영 리팩토링완료

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,28 +5,26 @@ import "./Header.scss";
 class Header extends Component {
   render() {
     return (
-      <>
-        <header className="Header">
-          <div className="HeaderContainer">
-            <div className="LeftIcon">
-              <button className="hamBtn">
-                <img src={configs.threeLine} alt="hamburger" />
-              </button>
-            </div>
-            <div className="CenterLogo">
-              <a href="/NewProducts">KAKAO FRIENDS</a>
-            </div>
-            <div className="RightIcon">
-              <button className="SearchBtn">
-                <img src={configs.searchBtnImg} alt="search" />
-              </button>
-              <button className="TransBtn">
-                <img src={configs.changeLanguageBtnImg} alt="earth" />
-              </button>
-            </div>
+      <header className="Header">
+        <div className="HeaderContainer">
+          <div className="HamburgerBox">
+            <button className="HamBtn">
+              <img src={configs.threeLine} alt="hamburger" />
+            </button>
           </div>
-        </header>
-      </>
+          <div className="CenterLogo">
+            <a href="/NewProducts">KAKAO FRIENDS</a>
+          </div>
+          <div className="SearchBox">
+            <button className="SearchBtn">
+              <img src={configs.searchBtnImg} alt="search" />
+            </button>
+            <button className="TransBtn">
+              <img src={configs.changeLanguageBtnImg} alt="earth" />
+            </button>
+          </div>
+        </div>
+      </header>
     );
   }
 }
