984c240551a6acf4d58f80b2ccad9573f8e947bd
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2020-12-17 19:44:05 +0900

    Modify: 리뷰 컴포넌트 리팩토링 진행

--- src/Pages/ProductDetail/Components/ProductReview.js
+++ src/Pages/ProductDetail/Components/ProductReview.js
@@ -3,7 +3,25 @@ import ReactStars from "react-stars";
 import * as configs from "../../../config.js";
 
 export class ProductReview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/data/ProductReview.json")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          reviews: res.reviewData,
+        });
+      });
+  }
+
   render() {
+    const { reviews } = this.state;
     return (
       <section className="ProductReview">
         <div>
@@ -33,35 +51,37 @@ export class ProductReview extends Component {
             <button>최신순</button>
           </div>
           <ul>
-            <li>
-              <div>
-                <div className="userName">이*연</div>
-                <div className="userReviewRate">
-                  <ReactStars
-                    className="ReactStars"
-                    count={5}
-                    value={1}
-                    size={18}
-                    half={true}
-                    edit={false}
-                    color1={"#D5D7E0"}
-                    color2={"#FF6582"}
-                    // onChange={}
-                  />
-                  <p>2020-12-12</p>
-                </div>
-                <div className="userReviewComment">
-                  불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~불합격~
-                </div>
-                <button className="seeMore">더보기</button>
-              </div>
-              <div className="reviewLike">
-                <button>좋아요</button>
-                <p>명이 좋아했어요</p>
-              </div>
-            </li>
+            {reviews &&
+              reviews.map((item, idx) => {
+                return (
+                  <li key={idx}>
+                    <div>
+                      <div className="userName">{item.userName}</div>
+                      <div className="userReviewRate">
+                        <ReactStars
+                          className="ReactStars"
+                          count={5}
+                          value={item.rateValue}
+                          size={18}
+                          half={true}
+                          edit={false}
+                          color1={"#D5D7E0"}
+                          color2={"#FF6582"}
+                        />
+                        <p>{item.date}</p>
+                      </div>
+                      <div className="userReviewComment">{item.comment}</div>
+                      <button className="seeMore">더보기</button>
+                    </div>
+                    <div className="reviewLike">
+                      <button>좋아요</button>
+                      <p>{item.likedNum}명이 좋아했어요</p>
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
-          <div className="CommentPagination">
+          <div className="CommentPageNum">
             <span>1</span>
           </div>
         </div>
