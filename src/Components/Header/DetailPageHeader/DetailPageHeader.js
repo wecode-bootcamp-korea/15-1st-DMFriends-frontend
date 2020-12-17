847b1373cb56f97ef117cc0ae1088abe98371210
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2020-12-17 23:52:44 +0900

    Modify: 링크 추가

--- src/Components/Header/DetailPageHeader/DetailPageHeader.js
+++ src/Components/Header/DetailPageHeader/DetailPageHeader.js
@@ -1,23 +1,37 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "./DetailPageHeader.scss";
 import * as configs from "../../../config.js";
 
 export class DetailPageHeader extends Component {
+  goToMain = () => {
+    this.props.history.push("/");
+  };
+
+  goToPrev = () => {
+    this.props.history.goBack();
+  };
+
+  goToSearch = () => {
+    this.state.history.push("/Search");
+  };
+
   render() {
+    const { goToMain, goToPrev, goToSearch } = this;
     return (
       <div className="DetailPageHeader">
         <div className="headerWrap">
           <div>
-            <button>
+            <button onClick={goToPrev}>
               <img src={configs.backwardBtnImg} alt="backwardBtnImg" />
             </button>
-            <button>
+            <button onClick={goToMain}>
               <img src={configs.toHomeBtnImg} alt="toHomeBtnImg" />
             </button>
           </div>
           <span>제품 상세</span>
           <div>
-            <button>
+            <button onClick={goToSearch}>
               <img src={configs.searchBtnImg} alt="searchBtnImg" />
             </button>
             <button>
@@ -30,4 +44,4 @@ export class DetailPageHeader extends Component {
   }
 }
 
-export default DetailPageHeader;
+export default withRouter(DetailPageHeader);
