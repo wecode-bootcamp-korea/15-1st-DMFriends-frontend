27b118d0548ce5987b58c6df0565d834a8c88194
Haiin05 <69622073+Haiin05@users.noreply.github.com>
2020-12-18 15:42:10 +0900

    Login 페이지 UI 구현

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -11,25 +11,36 @@ class Login extends React.Component {
   render() {
     return (
       <div className="Login">
-        <div className="frame">
-          <div className="title">kakao</div>
-          <form>
-            <input id="email" type="email" placeholder="카카오메일아이디, 이메일, 전화번호" />
-            <input id="pw" type="password" placeholder="비밀번호" />
-            <div>
-              <input type="checkbox" />
-              <span>로그인 상태 유지</span>
+        <div className="loginMain">
+          <div className="logo">로고</div>
+          <div className="frame">
+            <div className="title">kakao</div>
+            <form>
+              <input id="email" type="email" placeholder="카카오메일아이디, 이메일, 전화번호" />
+              <input id="pw" type="password" placeholder="비밀번호" />
+              <div>
+                <input type="checkbox" id="loginCheckbox" />
+                <label htmlFor="loginCheckbox">
+                  <span></span>
+                  로그인 상태 유지
+                </label>
+              </div>
+              <button className="loginBtn">로그인</button>
+              <div className="line">
+                <div></div>
+                <div>또는</div>
+                <div></div>
+              </div>
+              <button>QR코드 로그인</button>
+            </form>
+            <div className="signIn">
+              <Link>회원가입</Link>
+              <div>카카오계정 | 비밀번호찾기</div>
             </div>
-            <button>로그인</button>
-            <div>또는</div>
-            <button>QR코드 로그인</button>
-          </form>
-          <div className="">
-            <Link>회원가입</Link>
-            <div>카카오계정 | 비밀번호찾기</div>
           </div>
         </div>
         <footer>
+          <div></div>
           <div>
             <Link className="link" rel="stylesheet" href="#">
               이용약관
