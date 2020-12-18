43451c2df589b9ab168cbf4a0186c29bc26ae536
ingdaeho <62834439+ingdaeho@users.noreply.github.com>
2020-12-19 03:51:25 +0900

    Modify: 메뉴바 아이콘 추가

--- src/Components/Header/DetailPageHeader/DetailPageHeader.js
+++ src/Components/Header/DetailPageHeader/DetailPageHeader.js
@@ -4,26 +4,37 @@ import "./DetailPageHeader.scss";
 import * as configs from "../../../config.js";
 
 class DetailPageHeader extends Component {
-  goToMain = () => {
-    this.props.history.push("/");
-  };
-
   goToPrev = () => {
     this.props.history.goBack();
+  };
+
+  goToCategory = () => {
+    this.state.history.push("/Category");
+  };
+
+  goToMain = () => {
+    this.props.history.push("/");
   };
 
   goToSearch = () => {
     this.state.history.push("/Search");
   };
 
+  goToCart = () => {
+    this.state.history.push("/CartList");
+  };
+
   render() {
-    const { goToMain, goToPrev, goToSearch } = this;
+    const { goToMain, goToPrev, goToSearch, goToCategory, goToCart } = this;
     return (
       <div className="DetailPageHeader">
         <div className="headerWrap">
           <div>
             <button onClick={goToPrev}>
               <img src={configs.backwardBtnImg} alt="backwardBtnImg" />
+            </button>
+            <button onClick={goToCategory}>
+              <img src={configs.menuBtnImg} alt="toMenuBtnImg" />
             </button>
             <button onClick={goToMain}>
               <img src={configs.toHomeBtnImg} alt="toHomeBtnImg" />
@@ -33,6 +44,9 @@ class DetailPageHeader extends Component {
           <div>
             <button onClick={goToSearch}>
               <img src={configs.searchBtnImg} alt="searchBtnImg" />
+            </button>
+            <button onClick={goToCart}>
+              <img src={configs.bag} alt="toCartBtnImg" />
             </button>
             <button>
               <img src={configs.changeLanguageBtnImg} alt="changeLanguageBtnImg" />
